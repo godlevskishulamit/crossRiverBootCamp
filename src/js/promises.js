54246e31076a8ce2a3a71a15761a317dd0ed4e4e
Shani Schloss <shani3134376@gmail.com>
2022-07-04 18:23:29 +0300

    chained two functions

--- src/js/promises.js
+++ src/js/promises.js
@@ -1,8 +1,28 @@
 new Promise((res) => {
     setTimeout(() => {
-        res( Math.ceil((Math.random() * 10))
+        res(Math.ceil((Math.random() * 10))
         )
     }, 3000);
 }).then(
     res => alert("the number is: " + res)
-)
+);
+
+const makeAllCaps = (arr) => {
+    return arr.map(element => {
+        return element.charAt(0).toUpperCase() + element.substring(1).toLowerCase();
+    });
+}
+const sortWords = (arr) => {
+    return arr.sort();
+}
+const isTypeString = (currentValue) => typeof currentValue == "string";
+
+new Promise((res, rej) => {
+    const arr = [1, "two", "three"];
+    if (!arr.every(isTypeString))
+        rej("the array has to be only strings");
+    res(makeAllCaps(arr))
+})
+    .then(arr => sortWords(arr))
+    .then(arr => console.log(arr))
+    .catch(error => console.log(error));
