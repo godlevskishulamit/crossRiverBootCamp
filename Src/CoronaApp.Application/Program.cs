fc1199c3b46e8dc6c7aa53984775fc3e00bbcc39
Shira058 <sh0583232581@gmail.com>
2022-07-22 13:59:19 +0300

    add logging to the DB

--- Src/CoronaApp.Application/Program.cs
+++ Src/CoronaApp.Application/Program.cs
@@ -29,14 +29,18 @@ using CoronaApp.Api.Midllewares;
 using CoronaApp.Dal;
 using CoronaApp.Dal.Models;
 using CoronaApp.Services;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog();
 builder.Services.AddDbContext<CoronaAppDBContext>(item => item.UseSqlServer(builder.Configuration.GetConnectionString("home")));
 builder.Services.AddControllers();
 builder.Services.AddScoped<ILocationRepository, LocationRepository>();
@@ -49,17 +53,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+IConfigurationRoot configuration = new
+            ConfigurationBuilder().AddJsonFile("appsettings.json",
+            optional: false, reloadOnChange: true).Build();
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration
+            (configuration).CreateLogger();
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwaggerUI(c => {
+    app.UseSwaggerUI(c =>
+    {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiDemo v1");
         c.RoutePrefix = "swagger";
     });
     app.UseSwagger();
 }
-
+//app.UseSerilog();
 app.UseHandlerErrorsMiddleware();
 app.UseHttpsRedirection();
 
