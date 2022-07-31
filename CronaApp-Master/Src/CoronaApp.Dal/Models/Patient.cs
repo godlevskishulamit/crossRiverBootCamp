9f25a80ad88d5911c629c985d55aee477e2ef086
חנצ'י פינקל <213548084@mby.co.il>
2022-08-01 01:56:39 +0300

    change age to dateOfBirth

--- CronaApp-Master/Src/CoronaApp.Dal/Models/Patient.cs
+++ CronaApp-Master/Src/CoronaApp.Dal/Models/Patient.cs
@@ -15,6 +15,6 @@ namespace CoronaApp.Dal
         [MinLength(8)]
         public string Id { get; set; }
         public string Name { get; set; }
-        public int? Age { get; set; }
+        public DateTime? dateOfBirth { get; set; }
     }
 }
